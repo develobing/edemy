cbf5b69d2920aef0a151b9b59f872888aa8e020b
Robin <robin@Robins-MacBook-Pro-Intel.local>
2023-02-13 10:59:15 +0900

    Add bootstrap and antd

---
+++ client/pages/_app.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'antd/dist/antd.css';
+
+const App = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default App;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 const Index = () => {
   return (
     <>
-      <h1>Hello World</h1>
+      <h1 className="jumbotron text-center bg-primary">Hello World</h1>
       <p>from next.js...</p>
     </>
   );
